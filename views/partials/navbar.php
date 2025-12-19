f9847fb1a8964d377976cdd0203ec54060813d32
Olivier RATIEVILLE <olivier.ratieville@gmail.com>
2025-12-19 17:17:42 +0100

    Bug: Missing closing div in navbar.php

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -3,4 +3,5 @@
         <a href="<?= WEB_ROOT."/index.php" ?>">MyEnterprise</a>
         <a href="<?= WEB_ROOT."/employe/list-employe.php" ?>">Employes</a>
         <a href="<?= WEB_ROOT."/employe/add-employe.php" ?>">Recruter</a>
+    </div>
 </header>
